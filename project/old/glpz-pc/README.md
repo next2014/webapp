e3d8ebbf9eaf1987b2b41edd7d56577668dfdb6b
next2014 <83329265@qq.com>
2019-06-12 18:08:05 +0800

    0612

---
+++ project/old/glpz-pc/README.md
@@ -0,0 +1,6 @@
+#进入项目 
+#$cd fh_webapp
+#运行项目
+#$fh_webapp/ gulp
+#完成运行
+#访问网址 http://localhost:3000
---
+++ vue_gp/src/components/cart.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="hello">
+    <div>
+      <h1>{{ msg }}</h1>
+      <h2>分享快乐小城故事</h2>
+    </div>
+    <ul class="footer">
+      <li>
+        <a href="" target="_blank">
+          商城首页
+        </a>
+      </li>
+      <li>
+        <a href="" target="_blank">
+          产品分类
+        </a>
+      </li>
+      <li>
+        <a href="" target="_blank">
+          购物车
+        </a>
+      </li>
+      <li>
+        <a href="" target="_blank">
+          我的
+        </a>
+      </li>
+    </ul>
+   </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: '欧美城文化小镇'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #fd8905;
+  }
+  .hello{
+    width: 100%;
+    height: 100%;
+  }
+  .footer{
+    position:absolute;
+    bottom:0;
+    left: 0;
+    width: 100%;
+    height: 30px;
+    line-height: 30px;
+  }
+  .footer a{
+    text-decoration: none;
+    display: block;
+  }
+</style>
