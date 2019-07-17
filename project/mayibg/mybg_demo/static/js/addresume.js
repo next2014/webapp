431726b0396f637a768e1888d907cfd82046a2c7
next2014 <83329265@qq.com>
2019-07-17 15:08:59 +0800

    0717

---
+++ project/mayibg/mybg_demo/static/js/addresume.js
@@ -0,0 +1,409 @@
+var sexData = [];
+''
+sexData.push({
+    value: '1',
+    text: '男'
+})
+''
+sexData.push({
+    value: '2',
+    text: '女'
+})
+''
+var eduData = [];
+''
+eduData.push({
+    value: '9',
+    text: '高中以下'
+})
+''
+eduData.push({
+    value: '12',
+    text: '高中'
+})
+''
+eduData.push({
+    value: '13',
+    text: '中专'
+})
+''
+eduData.push({
+    value: '14',
+    text: '大专'
+})
+''
+eduData.push({
+    value: '15',
+    text: '本科'
+})
+''
+eduData.push({
+    value: '16',
+    text: '硕士'
+})
+''
+eduData.push({
+    value: '17',
+    text: '博士'
+})
+''
+var expData = [];
+''
+expData.push({
+    value: '205',
+    text: '无经验'
+})
+''
+expData.push({
+    value: '18',
+    text: '应届毕业生'
+})
+''
+expData.push({
+    value: '19',
+    text: '1年以上'
+})
+''
+expData.push({
+    value: '20',
+    text: '2年以上'
+})
+''
+expData.push({
+    value: '21',
+    text: '3年以上'
+})
+''
+expData.push({
+    value: '100',
+    text: '4年以上'
+})
+''
+expData.push({
+    value: '22',
+    text: '5年以上'
+})
+''
+expData.push({
+    value: '101',
+    text: '6年以上'
+})
+''
+expData.push({
+    value: '23',
+    text: '8年以上'
+})
+''
+expData.push({
+    value: '24',
+    text: '10年以上'
+})
+'';
+var hyData = [];
+''
+hyData.push({
+    value: '35',
+    text: '互联网/电子商务'
+})
+''
+hyData.push({
+    value: '837',
+    text: 'IT服务（系统/数据/维护）'
+})
+''
+hyData.push({
+    value: '835',
+    text: '计算机软件/硬件'
+})
+''
+hyData.push({
+    value: '862',
+    text: '交通/运输/物流'
+})
+''
+hyData.push({
+    value: '861',
+    text: '航空/航天研究与制造'
+})
+''
+hyData.push({
+    value: '860',
+    text: '医疗设备/器械'
+})
+''
+hyData.push({
+    value: '859',
+    text: '医药/生物工程'
+})
+''
+hyData.push({
+    value: '858',
+    text: '办公用品及设备'
+})
+''
+hyData.push({
+    value: '857',
+    text: '印刷/包装/造纸'
+})
+''
+hyData.push({
+    value: '856',
+    text: '仪器仪表及工业自动化'
+})
+''
+hyData.push({
+    value: '855',
+    text: '加工制造（原料加工/模具）'
+})
+''
+hyData.push({
+    value: '854',
+    text: '大型设备/机电设备/重工业'
+})
+''
+hyData.push({
+    value: '864',
+    text: '医疗/护理/美容/保健'
+})
+''
+hyData.push({
+    value: '865',
+    text: '酒店/餐饮'
+})
+''
+hyData.push({
+    value: '866',
+    text: '旅游/度假'
+})
+''
+hyData.push({
+    value: '875',
+    text: '农/林/牧/渔'
+})
+''
+hyData.push({
+    value: '874',
+    text: '学术/科研'
+})
+''
+hyData.push({
+    value: '873',
+    text: '政府/公共事业/非盈利机构'
+})
+''
+hyData.push({
+    value: '872',
+    text: '环保'
+})
+''
+hyData.push({
+    value: '871',
+    text: '电气/电力/水利'
+})
+''
+hyData.push({
+    value: '870',
+    text: '石油/石化/化工'
+})
+''
+hyData.push({
+    value: '869',
+    text: '能源/矿产/采掘/冶炼'
+})
+''
+hyData.push({
+    value: '868',
+    text: '娱乐/体育/休闲'
+})
+''
+hyData.push({
+    value: '867',
+    text: '媒体/出版/影视/文化传播'
+})
+''
+hyData.push({
+    value: '853',
+    text: '汽车/摩托车'
+})
+''
+hyData.push({
+    value: '852',
+    text: '工艺美术/收藏品/奢侈品'
+})
+''
+hyData.push({
+    value: '45',
+    text: '网络游戏'
+})
+''
+hyData.push({
+    value: '44',
+    text: '通信/电信/网络设备'
+})
+''
+hyData.push({
+    value: '43',
+    text: '通信/电信运营、增值服务'
+})
+''
+hyData.push({
+    value: '42',
+    text: '电子技术/半导体/集成电路'
+})
+''
+hyData.push({
+    value: '41',
+    text: '基金/证券/期货/投资'
+})
+''
+hyData.push({
+    value: '40',
+    text: '金融/保险/银行'
+})
+''
+hyData.push({
+    value: '38',
+    text: '信托/担保/拍卖/典当'
+})
+''
+hyData.push({
+    value: '37',
+    text: '房地产/建筑/建材/工程'
+})
+''
+hyData.push({
+    value: '36',
+    text: '家居/室内设计/装饰装潢'
+})
+''
+hyData.push({
+    value: '839',
+    text: '物业管理/商业中心'
+})
+''
+hyData.push({
+    value: '840',
+    text: '专业服务/咨询(财会/法律/人力资源等)'
+})
+''
+hyData.push({
+    value: '841',
+    text: '广告/会展/公关'
+})
+''
+hyData.push({
+    value: '850',
+    text: '教育/培训'
+})
+''
+hyData.push({
+    value: '849',
+    text: '租赁服务'
+})
+''
+hyData.push({
+    value: '848',
+    text: '零售/批发'
+})
+''
+hyData.push({
+    value: '847',
+    text: '贸易/进出口'
+})
+''
+hyData.push({
+    value: '846',
+    text: '耐用消费品（服饰/纺织/皮革/家具/家电）'
+})
+''
+hyData.push({
+    value: '845',
+    text: '快速消费品（食品/饮料/烟酒/日化）'
+})
+''
+hyData.push({
+    value: '844',
+    text: '外包服务'
+})
+''
+hyData.push({
+    value: '843',
+    text: '检验/检测/认证'
+})
+''
+hyData.push({
+    value: '842',
+    text: '中介服务'
+})
+''
+hyData.push({
+    value: '878',
+    text: '其他'
+})
+''
+var typeData = [];
+''
+typeData.push({
+    value: '57',
+    text: '不限'
+})
+''
+typeData.push({
+    value: '58',
+    text: '全职'
+})
+''
+typeData.push({
+    value: '59',
+    text: '兼职'
+})
+''
+var reportData = [];
+''
+reportData.push({
+    value: '45',
+    text: '随时到岗'
+})
+''
+reportData.push({
+    value: '46',
+    text: '1周以内'
+})
+''
+reportData.push({
+    value: '47',
+    text: '3周以内'
+})
+''
+reportData.push({
+    value: '48',
+    text: '1个月之内'
+})
+''
+reportData.push({
+    value: '50',
+    text: '3个月之内'
+})
+''
+reportData.push({
+    value: '51',
+    text: '不确定'
+})
+''
+var jobstatusData = [];
+''
+jobstatusData.push({
+    value: '114',
+    text: '已离职，寻求新工作'
+})
+''
+jobstatusData.push({
+    value: '115',
+    text: '在职，考虑更好职位'
+})
+''
+jobstatusData.push({
+    value: '116',
+    text: '其他'
+})
+''
